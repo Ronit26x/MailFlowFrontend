9573c74fbc9644c3ff236a10f99e9bcfb4fa7f35
Arnav Jain <arnav.aj999@gmail.com>
2023-09-22 11:19:47 +0530

    under construction added

--- src/app/pages/UnderConstruction/page.js
+++ src/app/pages/UnderConstruction/page.js
@@ -7,8 +7,8 @@ export default function UnderConstruction(){
     return(
         <div className="h-[90vh] w-full flex items-center justify-center">
             <div className="flex flex-col items-center">
-                <Lottie animationData={constructionAnimation} className="h-[60vh]" />
-                <p className="text-3xl font-bold">Sorry! We are still working on this page.</p>
+                <Lottie animationData={constructionAnimation} className="h-[60vh] -mt-10" />
+                <p className="text-3xl font-bold -mt-10">Sorry! We are still working on this page.</p>
             </div> 
         </div>
     );
